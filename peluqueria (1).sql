f07029b0b7fc97461f571c5b67ee8d21ab42158b
David.Banda <david.banda3824@utc.edu.ec>
2022-03-08 07:29:02 -0500

    Proyecto final V2

--- peluqueria (1).sql
+++ peluqueria (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-02-2022 a las 06:51:39
+-- Tiempo de generación: 08-03-2022 a las 13:28:16
 -- Versión del servidor: 10.4.21-MariaDB
 -- Versión de PHP: 7.3.31
 
@@ -40,7 +40,7 @@ CREATE TABLE `disponibilidad` (
 --
 
 INSERT INTO `disponibilidad` (`codigo_dis`, `fecha_dis`, `hora_inicio_dis`, `hora_fin_dis`, `creacion_dis`) VALUES
-(3, '2022-02-22', '07:00:00', '19:00:00', NULL);
+(12, '2021-03-18', '15:00:00', '17:00:00', NULL);
 
 -- --------------------------------------------------------
 
@@ -51,10 +51,10 @@ INSERT INTO `disponibilidad` (`codigo_dis`, `fecha_dis`, `hora_inicio_dis`, `hor
 CREATE TABLE `reserva` (
   `codigo_res` bigint(11) NOT NULL,
   `fecha_hora_inicio_res` datetime DEFAULT NULL,
-  `fecha_hora_fin_res` datetime DEFAULT NULL,
   `apellido_res` varchar(500) DEFAULT NULL,
   `nombre_res` varchar(500) DEFAULT NULL,
   `celular_res` varchar(15) DEFAULT NULL,
+  `estado_res` varchar(50) DEFAULT 'ACTIVO',
   `fk_codigo_ser` bigint(11) DEFAULT NULL,
   `creacion_res` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -63,9 +63,9 @@ CREATE TABLE `reserva` (
 -- Volcado de datos para la tabla `reserva`
 --
 
-INSERT INTO `reserva` (`codigo_res`, `fecha_hora_inicio_res`, `fecha_hora_fin_res`, `apellido_res`, `nombre_res`, `celular_res`, `fk_codigo_ser`, `creacion_res`) VALUES
-(2, '2022-02-22 10:30:00', NULL, 'Banda', 'David', '0969758158', 1, '2022-02-21 23:55:20'),
-(3, '2022-02-22 11:30:00', NULL, 'Gavilanez Guanoluisa', 'Diego David', '0987654321', 2, '2022-02-22 00:07:58');
+INSERT INTO `reserva` (`codigo_res`, `fecha_hora_inicio_res`, `apellido_res`, `nombre_res`, `celular_res`, `estado_res`, `fk_codigo_ser`, `creacion_res`) VALUES
+(9, '2022-03-06 16:30:00', 'Toapanta', 'Erika', '0987654321', 'ACTIVO', 2, '2022-03-04 21:34:28'),
+(10, '2022-03-06 17:30:00', 'Ruiz', 'David', '0987654321', 'ACTIVO', 1, '2022-03-05 11:56:38');
 
 -- --------------------------------------------------------
 
@@ -78,7 +78,6 @@ CREATE TABLE `servicios` (
   `nombre_ser` varchar(500) DEFAULT NULL,
   `descripcion_ser` varchar(3000) DEFAULT NULL,
   `precio_ser` varchar(500) DEFAULT NULL,
-  `tiempo_ser` time DEFAULT NULL,
   `foto_ser` varchar(500) DEFAULT NULL,
   `creacion_ser` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -87,9 +86,9 @@ CREATE TABLE `servicios` (
 -- Volcado de datos para la tabla `servicios`
 --
 
-INSERT INTO `servicios` (`codigo_ser`, `nombre_ser`, `descripcion_ser`, `precio_ser`, `tiempo_ser`, `foto_ser`, `creacion_ser`) VALUES
-(1, 'CORTE DE CABELLO', 'Cortada de cabellocon tijeras', '3', '00:30:00', 'f2076-f3.jpg', '2022-02-21 00:00:00'),
-(2, 'PERMANENTE RIZADO', 'Rizado permanente', '15', '01:00:00', '72c60-f4.jpg', '2022-02-21 00:00:00');
+INSERT INTO `servicios` (`codigo_ser`, `nombre_ser`, `descripcion_ser`, `precio_ser`, `foto_ser`, `creacion_ser`) VALUES
+(1, 'CORTE DE CABELLO HOMBRE', 'Cortada de cabellocon tijeras', '3', 'f2076-f3.jpg', '2022-02-21 00:00:00'),
+(2, 'PERMANENTE RIZADO', 'Rizado permanente', '15', '72c60-f4.jpg', '2022-02-21 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -113,7 +112,8 @@ CREATE TABLE `usuario` (
 --
 
 INSERT INTO `usuario` (`codigo_usu`, `usuario_usu`, `password_usu`, `estado_usu`, `perfil_usu`, `apellido_usu`, `nombre_usu`, `creacion_usu`) VALUES
-(1, 'david', 'e10adc3949ba59abbe56e057f20f883e', 'ACTIVO', 'ADMINISTRADOR', 'Banda', 'David', '2022-02-04 00:00:00');
+(17, 'david', 'e10adc3949ba59abbe56e057f20f883e', 'ACTIVO', 'ADMINISTRADOR', 'Banda', 'David', '2022-03-07 16:53:57'),
+(19, 'rafita', 'e10adc3949ba59abbe56e057f20f883e', 'ACTIVO', 'ADMINISTRADOR', 'David', 'Banda', '2022-03-07 22:36:37');
 
 --
 -- Índices para tablas volcadas
@@ -130,7 +130,7 @@ ALTER TABLE `disponibilidad`
 --
 ALTER TABLE `reserva`
   ADD PRIMARY KEY (`codigo_res`),
-  ADD UNIQUE KEY `fk_codigo_ser` (`fk_codigo_ser`);
+  ADD KEY `fk_codigo_ser` (`fk_codigo_ser`) USING BTREE;
 
 --
 -- Indices de la tabla `servicios`
@@ -152,25 +152,25 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `disponibilidad`
 --
 ALTER TABLE `disponibilidad`
-  MODIFY `codigo_dis` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `codigo_dis` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `reserva`
 --
 ALTER TABLE `reserva`
-  MODIFY `codigo_res` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `codigo_res` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT de la tabla `servicios`
 --
 ALTER TABLE `servicios`
-  MODIFY `codigo_ser` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `codigo_ser` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario`
 --
 ALTER TABLE `usuario`
-  MODIFY `codigo_usu` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `codigo_usu` bigint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- Restricciones para tablas volcadas
