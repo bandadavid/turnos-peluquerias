b7ee91d9f9d517211c5f23525f49e77e7faf717a
David.Banda <david.banda3824@utc.edu.ec>
2022-03-08 09:05:50 -0500

    Proyecto final V5

--- application/controllers/Paises.php
+++ application/controllers/Paises.php
@@ -80,8 +80,8 @@ class Paises extends CI_Controller
         $precio = "3.50";
         $foto = "foto.png";*/
         //$id = $this->input->post("codigo_res");
-        $nombre = $this->input->post("nombre_ser");
-        $continente = $this->input->post("descripcion_ser");
+        $nombre = $this->input->post("nombre_bd");
+        $continente = $this->input->post("continente_bd");
 
         $dataNuevoPais = array(
             "nombre_bd" => $nombre,
